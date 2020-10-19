1be6b3002d5f1c091405d6df1109245f67ba96b9
jeongin <12183385@inha.edu>
2020-10-20 02:57:25 +0900

    feat: 홈화면 상단 네비게이션 추가/ 정인

--- MagicSoraGodong/ViewControllers/HomeViewController.swift
+++ MagicSoraGodong/ViewControllers/HomeViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView:UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let category = ["의류","패션","인테리어"]
+        for i in 0..<category.count{
+            let label = UILabel()
+            label.text = category[i]
+            let xposion = (self.view.frame.width/6)*CGFloat(i)
+            label.frame = CGRect(x: xposion+15, y: 15, width:30 , height: 30)
+            
+            label.sizeToFit()
+            if i==0{
+                label.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+            }
+            scrollView.contentSize.width = self.view.frame.width*1.5
+            scrollView.addSubview(label)
+        } 
     }
 
 
 }
 
+ 
+ 
