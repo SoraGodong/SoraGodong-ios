33211a57d10b7933f36d704407d27c8878ea5acb
jeongin <12183385@inha.edu>
2020-10-20 12:34:45 +0900

    feat: homeviewController 카테고리 추가/정인

--- MagicSoraGodong/ViewControllers/HomeViewController.swift
+++ MagicSoraGodong/ViewControllers/HomeViewController.swift
@@ -10,27 +10,33 @@ import UIKit
 class HomeViewController: UIViewController {
     
     @IBOutlet weak var scrollView:UIScrollView!
+    let category = ["의류","패션","인테리어","가전"]
     
+    
+    //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        let category = ["의류","패션","인테리어"]
-        for i in 0..<category.count{
-            let label = UILabel()
-            label.text = category[i]
-            let xposion = (self.view.frame.width/6)*CGFloat(i)
-            label.frame = CGRect(x: xposion+15, y: 15, width:30 , height: 30)
-            
-            label.sizeToFit()
-            if i==0{
-                label.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-            }
-            scrollView.contentSize.width = self.view.frame.width*1.5
-            scrollView.addSubview(label)
-        } 
+        initUI()
     }
 
 
 }
 
- 
+//MARK: UI
+extension HomeViewController{
+    func initUI(){
+        //상단 메뉴 생성
+        for i in 0..<category.count{
+            let label = UILabel()
+            let xposion = (self.view.frame.width/6)*CGFloat(i)
+            
+            label.text = category[i]
+            label.frame = CGRect(x: xposion+15, y: 15, width:30 , height: 30)
+            label.sizeToFit()
+            
+            scrollView.contentSize.width = self.view.frame.width*1.5
+            scrollView.addSubview(label)
+        }
+    }
+}
  
---
+++ MagicSoraGodong/extensions/extension.swift
@@ -0,0 +1,8 @@
+//
+//  extension.swift
+//  MagicSoraGodong
+//
+//  Created by 이정인 on 2020/10/20.
+//
+
+import Foundation
