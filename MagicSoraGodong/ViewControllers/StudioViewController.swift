ffaf1b59671108c9d25c96ba196b0077e061dd13
younoah <kr_uno_10@naver.com>
2020-10-20 12:36:26 +0900

    feat: Add StudioViewController/younoah

---
+++ MagicSoraGodong/ViewControllers/StudioViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StudioViewController.swift
+//  MagicSoraGodong
+//
+//  Created by uno on 2020/10/20.
+//
+
+import UIKit
+
+class StudioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
