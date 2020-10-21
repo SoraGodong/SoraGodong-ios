478f0980482e8ed11df457227318c4172b56eedf
rose6649 <rose6640@likelion.org>
2020-10-22 01:28:49 +0900

    mypage reload/희주

---
+++ MagicSoraGodong/ViewControllers/ShowHostTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ShowHostTableViewCell.swift
+//  MagicSoraGodong
+//
+//  Created by hiju on 2020/10/21.
+//
+
+import UIKit
+
+class ShowHostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var showhostLabel: UILabel!
+
+}
