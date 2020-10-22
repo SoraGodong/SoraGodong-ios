4195e63016fdb907e33eb105417cf267304cec64
rose6649 <rose6640@likelion.org>
2020-10-22 23:59:09 +0900

    all mypage complete/희주

---
+++ MagicSoraGodong/ViewControllers/MyPageAllOfVideosTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MyPageAllOfVideosTableViewCell.swift
+//  MagicSoraGodong
+//
+//  Created by hiju on 2020/10/22.
+//
+
+import UIKit
+
+class MyPageAllOfVideosTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var profile: UIImageView!
+    @IBOutlet weak var videoTitle: UILabel!
+    @IBOutlet weak var videoItem: UILabel!
+    
+    
+    //MARK: UI
+    
+    func update(video:Video){
+        self.thumbnail.loadImage(url: video.thumbURL)
+        self.videoTitle.text = video.title
+        self.videoItem.text = video.itemtitle
+    }
+
+}
