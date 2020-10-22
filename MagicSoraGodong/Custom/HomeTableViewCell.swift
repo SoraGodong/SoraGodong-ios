b5fab946f7eb780bc9008f6ee90d46c43cfac817
jeongin <12183385@inha.edu>
2020-10-22 15:08:03 +0900

    chore: .gitattributes/정인

--- MagicSoraGodong/Custom/HomeTableViewCell.swift
+++ MagicSoraGodong/Custom/HomeTableViewCell.swift
@@ -21,7 +21,7 @@ class HomeTableViewCell: UITableViewCell {
         self.thumbnail.loadImage(url: video.thumbURL)
         //self.profile.loadImage(url: video.profile)
         self.videoTitle.text = video.title
-        self.videoItem.text = video.itemtitle
+        self.videoItem.text = video.subtitle
     }
 
     static let cellIdentifier = "homeCell"
