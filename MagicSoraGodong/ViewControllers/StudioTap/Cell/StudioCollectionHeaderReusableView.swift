0d80e68c40079eb2d6e5bdd332c951be1c431bea
younoah <kr_uno_10@naver.com>
2020-10-23 15:12:01 +0900

    feat: Update Ctegoty Menu Bar / 윤호

--- MagicSoraGodong/ViewControllers/StudioTap/Cell/StudioCollectionHeaderReusableView.swift
+++ MagicSoraGodong/ViewControllers/StudioTap/Cell/StudioCollectionHeaderReusableView.swift
@@ -9,14 +9,57 @@ import UIKit
 
 class StudioCollectionHeaderReusableView: UICollectionReusableView {
     
-    @IBOutlet weak var wholeButton: UIButton!
-    @IBOutlet weak var foodButton: UIButton!
+    // MARK:- Properties
+    @IBOutlet weak var categoryView: UIView!
     let categories = categoty.categories
-
+    var categoryButtons: [UIButton] = []
+    
+    // MARK:- Nib Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        configureCategory()
     }
     
+}
+
+// MARK:- Cofigure UI
+extension StudioCollectionHeaderReusableView {
+    
+    func configureCategory() {
+        
+        for index in 0 ..< categories.count {
+            let categoryButton = UIButton()
+            categoryButton.tag = index
+            categoryButton.setTitle(categories[index], for: .normal)
+            categoryButton.setTitleColor(.lightGray, for: .normal)
+            categoryButton.translatesAutoresizingMaskIntoConstraints = false
+            categoryButton.addTarget(self, action: #selector(touchUpCategoryButton(_:)), for: .touchUpInside)
+            categoryView.addSubview(categoryButton)
+            categoryButton.centerYAnchor.constraint(
+                equalTo: categoryView.centerYAnchor
+            ).isActive = true
+            categoryButtons.append(categoryButton)
+            categoryButton.leadingAnchor.constraint(
+                equalTo: index == 0 ? categoryView.leadingAnchor : categoryButtons[index - 1].trailingAnchor,
+                constant: 18).isActive = true
+        }
+    }
     
 }
+
+// MARK:- Methods
+extension StudioCollectionHeaderReusableView {
+    
+    @objc func touchUpCategoryButton(_ sender: UIButton) {
+        for index in 0 ..< categoryButtons.count {
+            if sender.tag == index {
+                categoryButtons[index].setTitleColor(.black, for: .normal)
+            } else {
+                categoryButtons[index].setTitleColor(.lightGray, for: .normal)
+            }
+        }
+    }
+    
+}
+
